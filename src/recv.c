4eed1c646b73f184ed3a26675f74b4745de95784
Oasis <xizhuo@gmail.com>
2018-07-28 19:43:12 +0800

    Update recv.c

--- src/recv.c
+++ src/recv.c
@@ -13,22 +13,19 @@ int main(int argc, char* argv[])//ip, port, from, pass
 {
 	int reg_res;
 	
-	if(argc != 5)
-	{
+	if(argc != 5){
 		fprintf(stderr, "Usage: %s ip port from pass !\n", argv[0]);
 		exit(-1);
 	}
 	
 	reg_res = start_receiver(argv[1], argv[2], argv[3], argv[4], notifyToClient);
 	
-	if(reg_res != 0)
-	{
+	if(reg_res != 0){
 		fprintf(stderr, "start_receiver failed reg_res: %d !\n", reg_res);
 		exit(-2);
 	}
 	
-	while(1)
-	{
+	while(1){
 		usleep(1*1000*1000);
 	}
 	
