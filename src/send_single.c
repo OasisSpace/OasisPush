a61b14b4f5e819eff8fb33b475674efa88fe8553
Oasis <xizhuo@gmail.com>
2018-07-28 19:43:31 +0800

    Update send_single.c

--- src/send_single.c
+++ src/send_single.c
@@ -6,15 +6,13 @@ int main(int argc, char* argv[])//ip, port, from, pass, to, message
 {
 	int reg_res;
 	
-	if(argc != 7)
-	{
+	if(argc != 7){
 		fprintf(stderr, "Usage: %s ip port from pass to message !\n", argv[0]);
 		exit(-2);
 	}
 	
 	reg_res = send_single_message(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6]);
-	if(reg_res != 0)
-	{
+	if(reg_res != 0){
 		fprintf(stderr, "send_single_message failed reg_res: %d !\n", reg_res);
 		exit(-1);
 	}
